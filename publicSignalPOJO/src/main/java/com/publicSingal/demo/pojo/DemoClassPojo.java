aa094f408a76f357e53069a2f88771fdfa4dd435
Xuyp <xuyp@bytter.com>
2017-08-10 11:46:36 +0800

    init

--- publicSignalPOJO/src/main/java/com/publicSingal/demo/pojo/DemoClassPojo.java
+++ publicSignalPOJO/src/main/java/com/publicSingal/demo/pojo/DemoClassPojo.java
@@ -1,5 +1,15 @@
 package com.publicSingal.demo.pojo;
 
 public class DemoClassPojo {
+    
+    private String name;
 
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String anName) {
+        this.name = anName;
+    }
+    
 }
